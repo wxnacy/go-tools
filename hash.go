8091a4c436bbd539caf49e03db7cb5b65e06e293
wxnacy <wxnacy@gmail.com>
2023-02-21 23:45:08 +0800

    feat(hash): 增加 Md5, Md5File 方法

---
+++ hash.go
@@ -0,0 +1,23 @@
+package gotool
+
+import (
+	"crypto/md5"
+	"encoding/hex"
+	"io/ioutil"
+)
+
+func Md5(str string) string {
+	h := md5.New()
+	h.Write([]byte(str))
+	return hex.EncodeToString(h.Sum(nil))
+}
+
+func Md5File(path string) (string, error) {
+	h := md5.New()
+	bytes, err := ioutil.ReadFile(path)
+	if err != nil {
+		return "", nil
+	}
+	h.Write(bytes)
+	return hex.EncodeToString(h.Sum(nil)), nil
+}
