16eaae6bc4708e43e79587e0dd469e26b47bb9a1
wxnacy <wxnacy@gmail.com>
2023-02-26 18:30:05 +0800

    refactor: 删除 files 包

--- files/constants.go
+++
@@ -1,8 +0,0 @@
-package files
-
-import "io/fs"
-
-const (
-	PermFile fs.FileMode = 0666
-	PermDir              = 0755
-)
--- files/file_test.go
+++
@@ -1,9 +0,0 @@
-package files
-
-import "testing"
-
-func TestFileExists(t *testing.T) {
-	if !FileExists("file.go") {
-		t.Error("file.go")
-	}
-}
